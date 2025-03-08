3cc6c24816383a4ede53d0ac44e05c5f46f2a81f
Rajat Dua <rajatdua1994@gmail.com>
2025-03-08 20:55:41 +0530

    2.5: Separate module step 10

--- part1/courseinfo/src/Course.jsx
+++ part1/courseinfo/src/Course.jsx
@@ -1,6 +1,29 @@
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      ))}
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((acc, part) => acc + part.exercises, 0);
+  return <b>total of {total} exercises</b>;
+};
 
 const Course = ({ course }) => {
   return (
--- part1/courseinfo/src/Total.jsx
+++
@@ -1,6 +0,0 @@
-const Total = ({ parts }) => {
-  const total = parts.reduce((acc, part) => acc + part.exercises, 0);
-  return <p>total of {total} exercises</p>;
-};
-
-export default Total;
